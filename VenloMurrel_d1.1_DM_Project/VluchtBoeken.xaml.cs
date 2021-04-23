82a1655bb8a8d5bd6658e256019526c260a98f1e
venlomj <r0781309@student.thomasmore.be>
2021-04-23 11:44:52 +0200

    Add project files.

---
+++ VenloMurrel_d1.1_DM_Project/VluchtBoeken.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VenloMurrel_d1._1_DM_Project
+{
+    /// <summary>
+    /// Interaction logic for VluchtBoeken.xaml
+    /// </summary>
+    public partial class VluchtBoeken : Window
+    {
+        public VluchtBoeken()
+        {
+            InitializeComponent();
+        }
+    }
+}
