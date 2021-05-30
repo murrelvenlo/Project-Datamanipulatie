2391288eb42fc5a5cad042e9f9ac4fb5a125bfb5
venlomj <r0781309@student.thomasmore.be>
2021-05-30 22:20:27 +0200

    Messagebox aangepast.

--- VenloMurrel_d1.1_DM_Project/CustomMessageBox.xaml.cs
+++ VenloMurrel_d1.1_DM_Project/CustomMessageBox.xaml.cs
@@ -26,17 +26,14 @@ namespace VenloMurrel_d1._1_DM_Project
 
         public void Toon(string tekst)
         {
-            
-            lblInhoud.Content = BerichtAanpassen(tekst);
+            lblInhoud.Content = tekst;
             this.ShowDialog();
 
         }
 
         public void Toon(string tekst, string titel)
         {
-            
-            lblInhoud.Content = BerichtAanpassen(tekst);
-            this.Title = titel; 
+            lblInhoud.Content = tekst; 
             this.ShowDialog();
 
         }
@@ -57,29 +54,7 @@ namespace VenloMurrel_d1._1_DM_Project
             btnOk.Background = Brushes.Red;
             
         }
-        //om de content er niet uit te laten vallen;
-        public string BerichtAanpassen(string bericht)
-        {
-            int count = 0; //Startpositie --> zolang het bericht groter of gelijk is aan de count: positie verhogen
-
-            while (count <= bericht.Length)
-            {
-                if (bericht.Length > 30)
-                {
-                    count += 30;
-                }
-                else if (bericht.Length > 60)
-                {
-                    count += 60;
-                }
-                else { count += 90; }
-                if (count > bericht.Length) //Als de count tocht groter is dan het bericht, wordt de lus verbroken
-                {
-                    break;
-                }
-                /*   bericht = bericht.Insert(count, "\n");*/ //bericht gelijkstellen aan het nieuwe bericht met nieuwe regel op de positie van count
-            }
-            return bericht;
-        }
+        
+        
     }
 }
